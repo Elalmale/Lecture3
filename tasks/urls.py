45bfa5a543565ff8c97a496c5c33bc5beb7a940d
Alva <elalmale@gmail.com>
2022-10-28 07:15:20 +0200

    Fin de lecture 3

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name="tasks"
 urlpatterns=[
     path("",views.lista,name="lista"),
     path("add",views.add,name="añadir")
--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,31 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django import forms
+from django.urls import reverse
 
-tasks=["correr","caminar","nadar"]
+
+
+
+class formumulario(forms.Form):
+    task=forms.CharField(label="Nueva actividad")
+    #prioridad=forms.IntegerField(label="prioridad", min_value=0,max_value=5)
 
 # Create your views here.
 
 def lista(request):
-    return render(request,"tasks/plantillalista.html",{"lista":tasks})
+    if "tasks" not in request.session:
+        request.session["tasks"]=[]
+    return render(request,"tasks/plantillalista.html",{"lista":request.session["tasks"]})
 
 def add(request):
-    return render(request,"tasks/add.html")
+    if request.method=="POST":
+        form=formumulario(request.POST)
+        if form.is_valid():
+            task=form.cleaned_data["task"]
+            request.session["tasks"] += [task]
+            return HttpResponseRedirect(reverse("tasks:lista"))
+        else:
+            return render(request,'tasks/add.html',{"form":form} )
+
+
+    return render(request,"tasks/add.html", {"form":formumulario()})
